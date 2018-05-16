fa2e8193248cb46bdd1a3e7b25fd34884f30e9e1
Pietro Di Bello <pierodibello@gmail.com>
2018-05-16 17:19:42 +0200

    use own docker hub account

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,11 +14,11 @@ node {
 
     stage("Build") {
         sh "${mvnHome}/bin/mvn package"
-        app = docker.build("pdincau/zip-service")
+        app = docker.build("pierodibello/zip-service")
     }
 
     stage("UAT vs Container") {
-        docker.image('pdincau/zip-service').withRun('-p 8081:8080') { c ->
+        docker.image('pierodibello/zip-service').withRun('-p 8081:8080') { c ->
             sh 'sleep 5'
             sh "${mvnHome}/bin/mvn -Dtest=\"*UAT\" -Dhost=localhost -Dport=8081 test"
         }
